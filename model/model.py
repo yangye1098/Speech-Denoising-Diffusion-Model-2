5dd5fa80bc989e1852945374fa6555f24aa20033
Ye <yangye1098@gmail.com>
2021-12-27 22:47:35 -0800

    fix different device

--- model/model.py
+++ model/model.py
@@ -41,7 +41,7 @@ class SDDM(BaseModel):
             assert batch_size==1, 'Batch size must be 1 to do continuous sampling'
             samples = [x]
             for t in tqdm(reversed(range(0, self.num_timesteps)), desc='sampling loop time step', total=self.num_timesteps):
-                noise_level = self.diffusion.get_noise_level(t)* torch.ones(batch_size)
+                noise_level = self.diffusion.get_noise_level(t)* torch.ones(batch_size, device=x.device)
                 predicted = self.denoise_network(x, y_t, noise_level)
                 y_t = self.diffusion.q_transition(y_t, t, predicted)
                 if t % sample_inter == 0:
@@ -51,7 +51,7 @@ class SDDM(BaseModel):
 
         else:
             for t in tqdm(reversed(range(0, self.num_timesteps)), desc='sampling loop time step', total=self.num_timesteps):
-                noise_level = self.diffusion.get_noise_level(t)* torch.ones(batch_size)
+                noise_level = self.diffusion.get_noise_level(t)* torch.ones(batch_size, device=x.device)
                 predicted = self.denoise_network(x, y_t, noise_level)
                 y_t = self.diffusion.q_transition(y_t, t, predicted)
 
