451ec42cd5523e53fa61bddcd5b6921cca35e9b8
Ye <yangye1098@gmail.com>
2022-03-02 15:45:18 -0800

    finish UNetModified2.py

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -77,7 +77,7 @@ class AudioDataLoader(BaseDataLoader):
     """
     def __init__(self, dataset,  batch_size, shuffle=True, validation_split=0.0, num_workers=1):
         self.dataset =dataset
-        super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers, collate_fn)
+        super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
 
 
 
