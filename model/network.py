21ba8ee9a0d27129e1a25a6e070f00b52e9e7003
Ye <yangye1098@gmail.com>
2022-03-02 15:45:56 -0800

    finish UNetModified2.py

--- model/network.py
+++ model/network.py
@@ -3,4 +3,5 @@ from .waveunet import Waveunet
 from .waveunet2 import Waveunet2
 from .waveunet3 import Waveunet3
 from .UNetModified import UNetModified
+from .UNetModified2 import UNetModified2
 from .tstnn import TSTNN
